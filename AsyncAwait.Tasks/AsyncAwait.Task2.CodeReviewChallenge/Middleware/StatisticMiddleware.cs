186cbc89eec5a167ba84c23d886d02d7f5509fcf
DESKTOP-LEL0JMC\Anton <anton.shheglov.1@gmail.com>
2023-04-24 04:58:47 +0300

    Implement task2./Async

--- AsyncAwait.Tasks/AsyncAwait.Task2.CodeReviewChallenge/Middleware/StatisticMiddleware.cs
+++ AsyncAwait.Tasks/AsyncAwait.Task2.CodeReviewChallenge/Middleware/StatisticMiddleware.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Threading;
 using System.Threading.Tasks;
 using AsyncAwait.Task2.CodeReviewChallenge.Headers;
 using CloudServices.Interfaces;
@@ -21,22 +20,18 @@ public class StatisticMiddleware
 
     public async Task InvokeAsync(HttpContext context)
     {
-        string path = context.Request.Path;
-
-        var staticRegTask = Task.Run(
-            () => _statisticService.RegisterVisitAsync(path)
-                .ConfigureAwait(false)
-                .GetAwaiter().OnCompleted(UpdateHeaders));
+        var staticRegTask = _statisticService.RegisterVisitAsync(context.Request.Path);
         Console.WriteLine(staticRegTask.Status); // just for debugging purposes
-
-        void UpdateHeaders()
-        {
-            context.Response.Headers.Add(
-                CustomHttpHeaders.TotalPageVisits,
-                _statisticService.GetVisitsCountAsync(path).GetAwaiter().GetResult().ToString());
-        }
-
-        Thread.Sleep(3000); // without this the statistic counter does not work
+        await staticRegTask.ConfigureAwait(false);
+        await UpdateHeadersAsync(context).ConfigureAwait(false);
         await _next(context);
+    }
+
+    private async Task UpdateHeadersAsync(HttpContext context)
+    {
+        var count = await _statisticService.GetVisitsCountAsync(context.Request.Path).ConfigureAwait(false);
+        context.Response.Headers.Add(
+            CustomHttpHeaders.TotalPageVisits,
+            count.ToString());
     }
 }
