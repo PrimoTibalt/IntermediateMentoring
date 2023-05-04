b170e85da34309dc73d94be297dd36e48992c4a7
DESKTOP-LEL0JMC\Anton <anton.shheglov.1@gmail.com>
2023-05-05 01:09:39 +0300

    Refactor tasks./Async
    Rework ValueTask code.
    Rework calculation to use async.

--- AsyncAwait.Tasks/AsyncAwait.Task1.CancellationTokens/Calculator.cs
+++ AsyncAwait.Tasks/AsyncAwait.Task1.CancellationTokens/Calculator.cs
@@ -1,11 +1,12 @@
 ﻿using System.Threading;
+using System.Threading.Tasks;
 
 namespace AsyncAwait.Task1.CancellationTokens;
 
 internal static class Calculator
 {
     // todo: change this method to support cancellation token
-    public static long Calculate(int n, CancellationToken token)
+    public async static Task<long> CalculateAsync(int n, CancellationToken token)
     {
         long sum = 0;
 
@@ -14,7 +15,7 @@ internal static class Calculator
             token.ThrowIfCancellationRequested();
             // i + 1 is to allow 2147483647 (Max(Int32)) 
             sum = sum + (i + 1);
-            Thread.Sleep(10);
+            await Task.Delay(10);
         }
 
         return sum;
