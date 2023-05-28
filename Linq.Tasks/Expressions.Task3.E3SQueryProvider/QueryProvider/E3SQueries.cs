8ed3227c3adcd144fb86592afed7c3ee8ae29cb2
TERMOPALE\anton <anton.shheglov.1@gmail.com>
2023-05-28 18:33:16 +0300

    Fix FtsRequestTranslatorTests./LINQ

---
+++ Linq.Tasks/Expressions.Task3.E3SQueryProvider/QueryProvider/E3SQueries.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Expressions.Task3.E3SQueryProvider.QueryProvider
+{
+    internal static class E3SQueries
+    {
+        public static readonly IDictionary<string, Func<string, string>> Methods = new Dictionary<string, Func<string, string>>
+        {
+            { nameof(StartsWith) , StartsWith },
+            { nameof(EndsWith) , EndsWith },
+            { nameof(Contains) , Contains },
+            { nameof(Equals) , Equals },
+        };
+
+        private static string StartsWith(string argument) => $"({argument}*)";
+
+        private static string EndsWith(string argument) => $"(*{argument})";
+
+        private static string Contains(string argument) => $"(*{argument}*)";
+
+        private static string Equals(string argument) => $"({argument})";
+    }
+}
