ea2ed76a6c73ae8bc674ffaae86be973d1f57a8d
TERMOPALE\anton <anton.shheglov.1@gmail.com>
2023-06-16 04:29:46 +0300

    Add rest task template./REST

---
+++ REST.Tasks/RestApi/Models/Category.cs
@@ -0,0 +1,9 @@
+﻿namespace RestApi.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public IList<Item> Items { get; set; } = new List<Item>();
+    }
+}
