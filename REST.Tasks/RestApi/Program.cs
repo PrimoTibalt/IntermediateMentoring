ecb05569424d867215a668ed3625bb2bd5d3f145
TERMOPALE\anton <anton.shheglov.1@gmail.com>
2023-06-21 03:39:44 +0300

    Complete Task1 with iis./HOST

--- REST.Tasks/RestApi/Program.cs
+++ REST.Tasks/RestApi/Program.cs
@@ -1,6 +1,9 @@
+using Microsoft.AspNetCore.HttpOverrides;
 using Microsoft.AspNetCore.Mvc;
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.WebHost.UseIIS();
 
 builder.Services.AddControllers((options) =>
 {
@@ -10,6 +13,11 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddResponseCaching();
 
 var app = builder.Build();
+
+app.UseForwardedHeaders(new ForwardedHeadersOptions
+{
+    ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto
+});
 
 app.UseHttpsRedirection();
 
